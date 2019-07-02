8835209117a2f43e9810adc85ef7f2ba82c20809
ZohaNsar <zohahaa@yahoo.com>
2019-07-02 05:25:35 +0430

    Brake evaluation method into 5 methods with different return type

--- src/Engine/Evaluate.java
+++ src/Engine/Evaluate.java
@@ -1,5 +1,11 @@
 package Engine;
 
+import java.util.ArrayList;
+
 abstract public class Evaluate {
-    public abstract int evaluate_line(String expression);
+    public abstract int evaluate_exp(ArrayList<String> expression);
+    public abstract ArrayList<String> evaluate_loop(ArrayList<String> expression);
+    public abstract void evaluate_vassignment(ArrayList<String> expression);
+    public abstract ArrayList<String> evaluate_conditional(ArrayList<String> expression);
+    public abstract String evaluate_out(ArrayList<String> expression);
 }
