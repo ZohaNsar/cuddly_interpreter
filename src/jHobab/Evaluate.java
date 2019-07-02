bb001640cf38c3d5dc0605891e53d046fcacc4d1
ZohaNsar <zohahaa@yahoo.com>
2019-07-02 05:40:20 +0430

    Add comments to each inherited method

--- src/jHobab/Evaluate.java
+++ src/jHobab/Evaluate.java
@@ -16,21 +16,26 @@ public class Evaluate extends Engine.Evaluate {
 //        System.out.println( e.evaluate_line("12 + 84 + 31"));
     }
 
+    //to evaluate arithmetic expressions coded with print()
     @Override
     public int evaluate_exp(ArrayList<String> expression) {
+
         return 0;
     }
 
+    //to print what is stated in the loop
     @Override
     public ArrayList<String> evaluate_loop(ArrayList<String> expression) {
         return null;
     }
 
+    //to assign a value to a specified type
     @Override
     public void evaluate_vassignment(ArrayList<String> expression) {
 
     }
 
+    //to print what is stated in conditional expression
     @Override
     public ArrayList<String> evaluate_conditional(ArrayList<String> expression) {
         return null;
